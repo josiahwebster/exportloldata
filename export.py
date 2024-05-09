ebc94454b07eadf1fc37aa29b0a2b5f79bd3ed68
josiahwebster <josiahwebster12@gmail.com>
2024-05-09 16:54:05 +1000

    grab PUUID from in game name + tag

--- export.py
+++ export.py
@@ -4,8 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+# ACCOUNT-V1 URL
 REGION_URL = 'https://asia.api.riotgames.com'
+# MATCH-V5 SEA URL
+MATCH_REGION_URL = 'https://sea.api.riotgames.com'
 
+request_url = f"{REGION_URL}/riot/account/v1/accounts/by-riot-id/proxysinged/oce?api_key={API_KEY}"
+response = requests.get(request_url)
 
-requests.get(REGION_URL + "/riot/account/v1/accounts/by-riot-id/'proxysinged'/oce")
+# Grab PUUID from in game name + tag
+# To be replaced with user input
+if response.status_code == 200:
+    data = response.json()
+    puuid = data['puuid']
+    gameName = data['gameName']
+    tagLine = data['tagLine']
+
+    print("PUUID:", puuid)
+    print("Game Name:", gameName)
+    print("Tag Line:", tagLine)
+else:
+    print("Error:", response.status_code)
 
