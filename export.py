cfabbf1cec35e2d38a33a55d4372c42765815d67
josiahwebster <josiahwebster12@gmail.com>
2024-05-09 16:57:18 +1000

    clean up

--- export.py
+++ export.py
@@ -9,20 +9,21 @@ REGION_URL = 'https://asia.api.riotgames.com'
 # MATCH-V5 SEA URL
 MATCH_REGION_URL = 'https://sea.api.riotgames.com'
 
-request_url = f"{REGION_URL}/riot/account/v1/accounts/by-riot-id/proxysinged/oce?api_key={API_KEY}"
-response = requests.get(request_url)
+# grab puuid from name + tag
+def grabID(gamename, tag):
+    request_url = f"{REGION_URL}/riot/account/v1/accounts/by-riot-id/{gamename}/{tag}?api_key={API_KEY}"
+    response = requests.get(request_url)
 
-# Grab PUUID from in game name + tag
-# To be replaced with user input
-if response.status_code == 200:
-    data = response.json()
-    puuid = data['puuid']
-    gameName = data['gameName']
-    tagLine = data['tagLine']
-
-    print("PUUID:", puuid)
-    print("Game Name:", gameName)
-    print("Tag Line:", tagLine)
-else:
-    print("Error:", response.status_code)
-
+    if response.status_code == 200:
+        data = response.json()
+        puuid = data['puuid']
+        gameName = data['gameName']
+        tagLine = data['tagLine']
+        
+        print("PUUID:", puuid)
+        print("Game Name:", gameName)
+        print("Tag Line:", tagLine)
+    else:
+        print("Error:", response.status_code)
+    
+grabID("proxysinged", "oce")
